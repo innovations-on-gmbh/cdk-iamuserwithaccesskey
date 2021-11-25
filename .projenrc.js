0f3192b8c8367b7560993109a291abd0dafe9a26
Markus Ellers <shotty1@users.noreply.github.com>
2021-11-25 17:59:48 +0100

    remove dependabot options for now

--- .projenrc.js
+++ .projenrc.js
@@ -23,7 +23,6 @@ const project = new AwsCdkConstructLibrary({
     '.idea',
   ],
   docgen: true,
-  dependabot: true,
 
   // cdkDependencies: undefined,        /* Which AWS CDK modules (those that start with "@aws-cdk/") does this library require when consumed? */
   // cdkTestDependencies: undefined,    /* AWS CDK modules required for testing. */
