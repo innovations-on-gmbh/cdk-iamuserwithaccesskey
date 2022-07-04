39c435d48269cef9459f94db2d12e72a8d89cdc0
Markus Ellers <shotty1@users.noreply.github.com>
2022-07-04 09:27:53 +0200

    fix: Add autoApprove Username

--- .projenrc.js
+++ .projenrc.js
@@ -39,7 +39,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   gitignore: ['.DS_Store', '.idea', '.vscode'],
   docgen: true,
   autoApproveUpgrades: true,
-  autoApproveOptions: { allowedUsernames: ['inno-projen[bot]'] },
+  autoApproveOptions: { allowedUsernames: ['inno-projen[bot]', 'inno-projen'] },
   autoApproveProjenUpgrades: true,
   depsUpgrade: true,
   renovatebot: false,
